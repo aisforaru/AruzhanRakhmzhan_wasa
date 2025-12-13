c9f77cccf7abc9c7e675edec143fb649e7e1e8bb
Aruzhan Rakhmzhan <rahmzana@gmail.com>
2025-12-13 12:00:01 +0100

    Remove all files

--- service/components/requests/conversation.go
+++
@@ -1,9 +0,0 @@
-package requests
-
-type GetConversationByIDRequest struct {
-	ConversationID string `json:"conversationId" validate:"required"`
-}
-
-type GetAllConversationsRequest struct {
-	// Add filters or pagination here if needed later
-}
